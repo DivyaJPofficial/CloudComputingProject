44263675503562035cdf5e49aae5759a249da995
DivyaJPofficial <divya.jp0112@gmail.com>
2022-11-27 16:03:29 -0600

    adding jenkins in the userdata

--- terraform/jenkinsinit.sh
+++ terraform/jenkinsinit.sh
@@ -11,3 +11,4 @@ sudo yum install -y git
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+sudo service start jenkins
