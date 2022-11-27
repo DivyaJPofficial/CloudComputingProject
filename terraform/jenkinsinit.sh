970408bc0797256a3a8a2f1d3cf47de1a92d1948
DivyaJPofficial <111369917+DivyaJPofficial@users.noreply.github.com>
2022-11-27 17:13:23 -0600

    Update jenkinsinit.sh

--- terraform/jenkinsinit.sh
+++ terraform/jenkinsinit.sh
@@ -6,9 +6,9 @@ sudo amazon-linux-extras install epel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins -y
+sudo systemctl enable jenkins
 sudo service jenkins start
 sudo yum install -y git
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
-sudo service start jenkins
